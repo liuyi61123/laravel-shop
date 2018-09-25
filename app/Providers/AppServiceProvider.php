b274bd88894b87bd28144a28f06ad653a6467541
liuyi <475793342@qq.com>
2018-09-25 14:22:06 +0800

    安装开发部分所需插件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //时间格式转换
+        \Carbon\Carbon::setLocale('zh');
+        //数据库默认字符串长度191
+        Schema::defaultStringLength(191);
     }
 
     /**
