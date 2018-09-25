507b437e3fd70111b73b4b0a7c4acfec9ab9c745
liuyi <475793342@qq.com>
2018-09-25 14:59:04 +0800

    用户注册登录与邮箱验证

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,16 @@
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+
+    });
+});
+
+
