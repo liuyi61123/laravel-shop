6c236eb78ff8697ac9f1d16dcd447532d8469e56
liuyi <475793342@qq.com>
2018-09-25 15:35:53 +0800

    发送激活邮箱链接

---
+++ app/Listeners/RegisteredListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Auth\Events\Registered;
+
+class RegisteredListener
+{
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        // 获取到刚刚注册的用户
+        $user = $event->user;
+        // 调用 notify 发送通知
+        $user->notify(new EmailVerificationNotification());
+    }
+}
