42c3359bfccb2f7340dae66dd3d73cbfcb6593eb
liuyi <475793342@qq.com>
2018-09-26 14:43:14 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        //提交订单
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
 
     });
 });
