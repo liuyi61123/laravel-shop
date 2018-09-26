3ebd6ca3ec9b1feb847388ef5260ca1f13966192
liuyi <475793342@qq.com>
2018-09-26 13:54:46 +0800

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::group(['middleware' => 'auth'], function() {
         //收藏与取消
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        //购物车
+        Route::post('cart', 'CartController@add')->name('cart.add');
 
     });
 });
