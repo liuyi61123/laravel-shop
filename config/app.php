fb06fc34685eb4b0a54c60109d33c9155e87ea0d
liuyi <475793342@qq.com>
2018-10-16 15:33:41 +0800

    添加代理通道地址

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'proxy_url' => env('PROXY_APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
